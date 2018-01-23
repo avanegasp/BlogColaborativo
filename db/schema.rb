1f38618875f55b7db928e1d6fab209eed4e48724
avanegasp <arco527@gmail.com>
2018-01-23 16:20:20 -0500

    Asociations done

--- db/schema.rb
+++ db/schema.rb
@@ -28,19 +28,7 @@ ActiveRecord::Schema.define(version: 20180123193057) do
     t.date "database"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
     t.integer "user_id"
-    t.index ["email"], name: "index_posts_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_posts_on_reset_password_token", unique: true
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
