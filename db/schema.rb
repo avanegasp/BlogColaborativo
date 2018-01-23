e7e92f6747cbc3c3ec09cd007af4f4e072926095
avanegasp <arco527@gmail.com>
2018-01-23 15:21:12 -0500

    Bug in asociations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123165441) do
+ActiveRecord::Schema.define(version: 20180123193057) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -28,8 +38,10 @@ ActiveRecord::Schema.define(version: 20180123165441) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.integer "user_id"
     t.index ["email"], name: "index_posts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_posts_on_reset_password_token", unique: true
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
