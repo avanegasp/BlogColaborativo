36b497d0ec94c84802ba7fe67519fe3b4bca6953
avanegasp <arco527@gmail.com>
2018-01-24 15:17:39 -0500

    Comments in post ready

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+        redirect_to post_path(@post.id)
+      else
+        render '/posts/show'
+  end
+end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body).merge(user_id: current_user.id)
+  end
+end
