7370c6fd0667100e458004d46344ab6d4a1929d3
avanegasp <arco527@gmail.com>
2018-01-25 20:28:17 -0500

    Add in model post dependent:destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :comments, dependent: :destroy
+
   validates :title, presence: true
   validates :body, presence: true
   validates :body,  length: { minimum: 250 }
