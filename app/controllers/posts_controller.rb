b3757e2e5341ea190c0c3edb67a68d45e3fa2846
Angie <arco527@gmail.com>
2019-05-22 11:06:01 -0500

    Add styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user! , except: [:index, :show]
 
   def index
-    @post = Post.all
+    @posts = Post.all
   end
 
   def show
