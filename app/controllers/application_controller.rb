187a398e14b16f3910dfa226c3cd21bbee4f9135
Angie <arco527@gmail.com>
2019-05-24 11:27:58 -0500

    Fixing bug to see pictures in heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :debug
+
+
+  def debug
+    Rails.logger.info("Current User is: #{current_user.inspect}")
+  end
+
+
 end
