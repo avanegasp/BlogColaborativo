5f2d854d4dd2f67c029d4761bddb1e5e98d698ef
Angie <arco527@gmail.com>
2019-05-31 11:43:00 -0500

    Add two field to user form last_name and first_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :add_params, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  protected
+    def add_params
+      devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name])
+    end
+
 end
