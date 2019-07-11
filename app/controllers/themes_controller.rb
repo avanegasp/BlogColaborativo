2b5e912807f975fd070e75e769f6777106267ea3
Angie <arco527@gmail.com>
2019-07-11 11:37:36 -0500

    Button redirect

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,7 +1,9 @@
 class ThemesController < ApplicationController
 
   def theme_posts_index
+
      @theme = Theme.find(params[:theme_id])
      @posts = @theme.posts.order("id DESC").all
+     @themes = Theme.all
   end
 end
